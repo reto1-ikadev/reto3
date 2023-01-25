63e9382966bf7f092c960767d7490a098c90b50e
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2023-01-25 14:07:52 +0100

    solucionado merge

--- resources/views/coordinador/index.blade.php
+++ resources/views/coordinador/index.blade.php
@@ -95,43 +95,7 @@
         </div>
 
 
-        <div class="row">
-            <div class="col">
-                <footer class="text-center text-white" style="background-color: #f1f1f1;">
-                    <!-- Grid container -->
-                    <div class="container pt-4">
-                        <!-- Section: Social media -->
-                        <section class="mb-4">
-                            <!-- Facebook -->
-                            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">Facebook</a>
-
-                            <!-- Twitter -->
-                            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">Twitter</a>
-
-                            <!-- Google -->
-                            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">Google</a>
-
-                            <!-- Instagram -->
-                            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">Instagram</a>
-
-                            <!-- Linkedin -->
-                            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">Linkedin</a>
-                            <!-- Github -->
-                            <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="#!" role="button" data-mdb-ripple-color="dark">Github</a>
-                        </section>
-                        <!-- Section: Social media -->
-                    </div>
-                    <!-- Grid container -->
-
-                    <!-- Copyright -->
-                    <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-
-                        <a class="text-dark" href="https://deusto.com/">Deusto</a>
-                    </div>
-                    <!-- Copyright -->
-                </footer>
-            </div>
-        </div>
+        
 
 
 
