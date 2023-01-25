d58064f7db0cfcab09fe9d63b36cbcfa2aafdc11
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2023-01-25 10:50:43 +0100

    collapse

--- resources/views/coordinador/index.blade.php
+++ resources/views/coordinador/index.blade.php
@@ -76,57 +76,70 @@
         <div class="row justify-content-center">
             <div class="col-10">
                 @if(!isset($estudiantes)&&!isset($empresas)&&!isset($tutoresAcademicos)&&!isset($tutoresEmpresa))
-                <div id="carouselExampleFade" class = "carousel slide carousel-fade">
-                    <div class = "carousel-inner">
-                        <div class = "carousel-item active">
-                            <img src = "{{ asset('images/deusto3.jpg') }}" class="d-block w-100" alt="...">
+                <div id="carouselExampleFade" class="carousel slide carousel-fade">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{ asset('images/deusto3.jpg') }}" class="d-block w-100" alt="...">
                         </div>
-                        <div class = "carousel-item">
-                            <img src = "{{ asset('images/campusVitoria.jpg') }}" class="d-block w-100" alt="...">
+                        <div class="carousel-item">
+                            <img src="{{ asset('images/campusVitoria.jpg') }}" class="d-block w-100" alt="...">
                         </div>
-                        <div class = "carousel-item">
-                            <img src = "{{ asset('images/deusto2.png') }}" class="d-block w-100" alt="...">
+                        <div class="carousel-item">
+                            <img src="{{ asset('images/deusto2.png') }}" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <button class = "carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                        <span class = "carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class = "visually-hidden">Previous</span>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class = "carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                        <span class = "carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class = "visually-hidden">Next</span>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
                     </button>
                 </div>
                 @else
-                    @switch($tipo)
-                        @case('estudiante')
-                            <table class="table mt-2">
-                                <tr>
-                                    <td>Nombre</td>
-                                </tr>
-                            @foreach($estudiantes as $estudiante)
-                                <tr>
-                                    <td><a href="{{ route('estudiantes.show', $estudiante) }}"> {{$estudiante}}</a></td>
-                                </tr>
-                            @endforeach
-                            </table>
-                            @break
-                        @case('empresa')
-                            @foreach($empresas as $empresa)
-                                <h1> {{$empresa}}</h1>
-                            @endforeach
-                            @break
-                        @case('tutoresAcademicos')
-                            @foreach($tutoresAcademicos as $tutorAcademico)
-                                <h1> {{$tutorAcademico}}</h1>
-                            @endforeach
-                            @break
-                        @case('tutoresEmpresa')
-                            @foreach($tutoresEmpresa as $tutorEmpresa)
-                                <h1> {{$tutorEmpresa}}</h1>
-                            @endforeach
-                            @break
-                    @endswitch
+                @switch($tipo)
+                @case('estudiante')
+                <table class="table mt-2">
+                    <tr>
+                        <td>Nombre</td>
+                    </tr>
+                    @foreach($estudiantes as $estudiante)
+                    <tr>
+                        <td><a href="{{ route('estudiantes.show', $estudiante) }}"> {{$estudiante}}</a></td>
+                    </tr>
+                    @endforeach
+                </table>
+                @break
+                @case('empresa')
+                
+                    @foreach($empresas as $empresa)
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id = "flush-headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                {{$empresa}}
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                        </div>
+                    </div>
+                </div>
+
+                @endforeach
+                @break
+                @case('tutoresAcademicos')
+                @foreach($tutoresAcademicos as $tutorAcademico)
+                <h1> {{$tutorAcademico}}</h1>
+                @endforeach
+                @break
+                @case('tutoresEmpresa')
+                @foreach($tutoresEmpresa as $tutorEmpresa)
+                <h1> {{$tutorEmpresa}}</h1>
+                @endforeach
+                @break
+                @endswitch
                 @endif
             </div>
         </div>
