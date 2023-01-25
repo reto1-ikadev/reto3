b022f323c4106a50b9670cfd12a204bbda92d812
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2023-01-25 08:59:02 +0100

    Menu terminado

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ url('/css/dist/css/bootstrap.css') }}">
     <style>
-
+        
     </style>
 </head>
 
@@ -19,7 +19,7 @@
         <div class="row">
             <div class="col">
                 <nav class="navbar bg-primary "><!-- navegador -->
-                    <img src="{{ asset('images/logo.svg') }}" class="h-25 w-25" alt="">
+                    <img src="{{ asset('images/logo.svg') }}" id="logoCab" class="img-fluid h-20 w-25" alt="">
                     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -36,6 +36,12 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Empresas</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Tutores acad&eacute;micos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Tutores de empresa</a>
+                                </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Dar de alta
@@ -43,11 +49,19 @@
                                     <ul class="dropdown-menu">
                                         <li><a class="dropdown-item" href="#">Coordinador</a></li>
                                         <li><a class="dropdown-item" href="#">Tutor</a></li>
-                                        <li>
-                                            <hr class="dropdown-divider">
-                                        </li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        <li><a class="dropdown-item" href="#">Tutor de empresa</a></li>
+                                        <li><a class="dropdown-item" href="#">Alumnos</a></li>
                                     </ul>
+                                </li>
+                                
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Estad&iacute;sticas</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Cambiar modo</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Salir</a>
                                 </li>
                             </ul>
                         </div>
