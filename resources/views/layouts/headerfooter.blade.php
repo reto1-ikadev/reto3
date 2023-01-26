9287b53a9ca72b96ddb2358295f154bd976f8855
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2023-01-26 10:53:18 +0100

    un solo fichero bootstrap

--- resources/views/layouts/headerfooter.blade.php
+++ resources/views/layouts/headerfooter.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('titulo')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ url('/css/dist/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ url('/css/dist/css/styles.css') }}">
+    
     <style>
         
     </style>
