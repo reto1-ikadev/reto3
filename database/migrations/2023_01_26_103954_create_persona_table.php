3063d0ae88ff4ac1e7dad0db00f9a105d4cc022a
aritzcas <aritzcas@gmail.com>
2023-01-26 12:23:53 +0100

    migraciones

---
+++ database/migrations/2023_01_26_103954_create_persona_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id('id_persona');
+            $table->string('nombre', 50);
+            $table->string('apellidos',100);
+            $table->string('dni', 9);
+            $table->string('telefono', 9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ database/migrations/2023_01_26_105433_create_tutor_academico_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       //create child table
+        Schema::create('tutores_academicos', function (Blueprint $table) {
+            $table->foreignId('id_tutor_academico')->constrained('personas')->onDelete('cascade');
+            $table->string('telefono_academico', 9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutor_academico');
+    }
+};
