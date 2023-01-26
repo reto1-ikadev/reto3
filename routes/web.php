9c8013aa05c66c33f4215fbeb042e55ffc1cffdc
aritzcas <aritzcas@gmail.com>
2023-01-26 12:35:21 +0100

    Solucion routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CoordinadorController;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\TutorAcademicoController;
+use App\Http\Controllers\TutorEmpresaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Rutas del coordinador
+Route::get('/cordinador', [CoordinadorController::class, 'index'])->name('coordinador.index');
+
+//Rutas de los estudiantes
+Route::get('/estudiantes',[EstudianteController::class,'index'])->name('estudiantes.index');
+Route::get('/estudiantes/{estudiante}',[EstudianteController::class,'show'])->name('estudiantes.show');
+
+//Rutas de las empresas
+Route::get('/empresas',[EmpresaController::class,'index'])->name('empresas.index');
+
+//Rutas de los tutores academicos
+Route::get('/tutoresAcademicos',[TutorAcademicoController::class,'index'])->name('tutoresAcademicos.index');
+
+//Rutas de los tutores de empresa
+Route::get('/tutoresEmpresa',[TutorEmpresaController::class,'index'])->name('tutoresEmpresa.index');
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
