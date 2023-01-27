52843a0b513c10d1d3917077c511695e0a8c4eb7
Ikeregibide <iker.gomez.rodriguez@ikasle.egibide.org>
2023-01-27 10:58:42 +0100

    cambios carpeta

--- resources/views/layouts/headerfooter.blade.php
+++ resources/views/layouts/headerfooter.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/dist/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
     
     <style>
         
