1379629af5358cd4eaeba00ef223263b878e7e12
aritzcas <aritzcas@gmail.com>
2023-01-27 09:30:06 +0100

    Migrates

--- database/migrations/2023_01_26_123532_create_colificaciones_table.php
+++ database/migrations/2023_01_26_123532_create_colificaciones_table.php
@@ -15,12 +15,14 @@ return new class extends Migration
     {
         Schema::create('calificaciones_historial', function (Blueprint $table) {
             $table->id('id_calificacion');
-            $table->foreignId('id_evaluacion_diario')->constrained('evaluacion_diario')->onDelete('cascade');
-            $table->foreignId('id_evaluacion_empresa')->constrained('evaluacion_empresa')->onDelete('cascade');
-            $table->foreignId('id_alumno')->constrained('alumnos')->onDelete('cascade');
-            $table->foreignId('id_tutor_academico')->constrained('tutores_academicos')->onDelete('cascade');
-            $table->foreignId('id_tutor_empresa')->constrained('tutores_empresas')->onDelete('cascade');
-            $table->foreignId('id_curso')->constrained('cursos')->onDelete('cascade');
+            $table->foreignId('id_evaluacion_diario')->constrained('evaluacion_diario')->onDelete('set null');
+            $table->foreignId('id_evaluacion_empresa')->constrained('evaluacion_empresa')->onDelete('set null');
+            $table->foreignId('id_alumno')->constrained('alumnos')->onDelete('set null');
+            $table->foreignId('id_tutor_academico')->constrained('tutores_academicos')->onDelete('set null');
+            $table->foreignId('id_tutor_empresa')->constrained('tutores_empresas')->onDelete('set null');
+            $table->foreignId('id_curso')->constrained('cursos')->onDelete('set null');
+
+            $table->foreignId('id_ano_academico')->constrained('ano_academico')->onDelete('set null');
             $table->timestamps();
         });
     }
