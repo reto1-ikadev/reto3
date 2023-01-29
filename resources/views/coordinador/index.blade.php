890085382e86788afeb2f72abba830a4683e117d
aritzcas <aritzcas@gmail.com>
2023-01-29 22:46:16 +0100

    Seeders

--- resources/views/coordinador/index.blade.php
+++ resources/views/coordinador/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.headerfooter')
 @section('content')
 
-        <!--CARROUSEL o TABLAS-->
+    <!--CARROUSEL o TABLAS-->
 
-        <div class="row justify-content-center mt-5">
-            <div class="col-10">
-                @if(!isset($estudiantes)&&!isset($empresas)&&!isset($tutoresAcademicos)&&!isset($tutoresEmpresa))
+    <div class="row justify-content-center mt-5">
+        <div class="col-10">
+            @if(!isset($estudiantes)&&!isset($empresas)&&!isset($tutoresAcademicos)&&!isset($tutoresEmpresa))
                 <div id="carouselExampleFade" class="carousel slide carousel-fade">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -27,7 +27,7 @@
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
-                @else
+            @else
                 <h2>{{$tipo}}</h2>
                 <form id="filtrosEstudiantes" action="" method="post">
                     <div class="row mb-5" id="filtrosEst">
@@ -63,87 +63,87 @@
                             <input class="btn btn-primary" type="submit" value="Filtrar">
                         </div>
                     </div>
-                    
-                    
-                </form>
+
+
+                </form
                 @switch($tipo)
-                @case('estudiante')
-                <table class="table mt-2">
-                    <thead>
-                        <td>Nombre</td>
-                        <td>Apellido</td>
-                        <td>Grado</td>
-                        <td>Curso</td>
-                        <td>Empresa</td>
-                        <td>Detalles</td>
-                    </thead>
-                    @foreach($estudiantes as $estudiante)
-                    <tr>
-                        <td> {{$estudiante}}</td>
-                        <td>El apellido</td>
-                        <td>El grado</td>
-                        <td>El curso</td>
-                        <td>La Empresa</td>
-                        <td><a href="{{ route('estudiantes.show', $estudiante) }}"> Ver</a></td>
-                    </tr>
-                    @endforeach
-                </table>
-                @break
-                @case('empresa')
-                @foreach($empresas as $empresa)
-                    <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id = "flush-headingOne{{$empresa}}">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$empresa}}" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    {{$empresa}}
-                                </button>
-                            </h2>
-                            <div id="flush-collapseOne{{$empresa}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$empresa}}" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">Aqu&iacute; se muestran los datos de la empresa</div>
+                    @case('estudiante')
+                        <table class="table mt-2">
+                            <thead>
+                            <td>Nombre</td>
+                            <td>Apellido</td>
+                            <td>Grado</td>
+                            <td>Curso</td>
+                            <td>Empresa</td>
+                            <td>Detalles</td>
+                            </thead>
+                            @foreach($estudiantes as $estudiante)
+                                <tr>
+                                    <td> {{$estudiante}}</td>
+                                    <td>El apellido</td>
+                                    <td>El grado</td>
+                                    <td>El curso</td>
+                                    <td>La Empresa</td>
+                                    <td><a href="{{ route('estudiantes.show', $estudiante) }}"> Ver</a></td>
+                                </tr>
+                            @endforeach
+                        </table>
+                        @break
+                    @case('empresa')
+                        @foreach($empresas as $empresa)
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id = "flush-headingOne{{$empresa}}">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$empresa}}" aria-expanded="false" aria-controls="flush-collapseOne">
+                                            {{$empresa}}
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne{{$empresa}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$empresa}}" data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">Aqu&iacute; se muestran los datos de la empresa</div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                @endforeach
-                @break
-                @case('tutoresAcademicos')
-                @foreach($tutoresAcademicos as $tutorAcademico)
-                    <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id = "flush-headingOne{{$tutorAcademico}}">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$tutorAcademico}}" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    {{$tutorAcademico}}
-                                </button>
-                            </h2>
-                            <div id="flush-collapseOne{{$tutorAcademico}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$tutorAcademico}}" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">Aqu&iacute; se muestran los datos de la empresa</div>
+                        @endforeach
+                        @break
+                    @case('tutoresAcademicos')
+                        @foreach($tutoresAcademicos as $tutorAcademico)
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id = "flush-headingOne{{$tutorAcademico}}">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$tutorAcademico}}" aria-expanded="false" aria-controls="flush-collapseOne">
+                                            {{$tutorAcademico}}
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne{{$tutorAcademico}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$tutorAcademico}}" data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">Aqu&iacute; se muestran los datos de la empresa</div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                @endforeach
-                @break
-                @case('tutoresEmpresa')
-                @foreach($tutoresEmpresa as $tutorEmpresa)
-                    <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id = "flush-headingOne{{$tutorEmpresa}}">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$tutorEmpresa}}" aria-expanded="false" aria-controls="flush-collapseOne">
-                                    {{$tutorEmpresa}}
-                                </button>
-                            </h2>
-                            <div id="flush-collapseOne{{$tutorEmpresa}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$tutorEmpresa}}" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">Aqu&iacute; se muestran los datos de la empresa</div>
+                        @endforeach
+                        @break
+                    @case('tutoresEmpresa')
+                        @foreach($tutoresEmpresa as $tutorEmpresa)
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id = "flush-headingOne{{$tutorEmpresa}}">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne{{$tutorEmpresa}}" aria-expanded="false" aria-controls="flush-collapseOne">
+                                            {{$tutorEmpresa}}
+                                        </button>
+                                    </h2>
+                                    <div id="flush-collapseOne{{$tutorEmpresa}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne{{$tutorEmpresa}}" data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">Aqu&iacute; se muestran los datos de la empresa</div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                @endforeach
-                @break
+                        @endforeach
+                        @break
                 @endswitch
-                @endif
-            </div>
+            @endif
         </div>
+    </div>
 
 
-        
+
 
 
 
@@ -151,5 +151,5 @@
 
     </div>
 
-        
+
 @endsection
