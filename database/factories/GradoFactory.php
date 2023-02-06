8ef3bd1617e4d4d5511cdb7eae6eb22f8ba87104
Ikeregibide <iker.gomez.rodriguez@ikasle.egibide.org>
2023-02-06 10:00:59 +0100

    axio

--- database/factories/GradoFactory.php
+++ database/factories/GradoFactory.php
@@ -18,7 +18,7 @@ class GradoFactory extends Factory
     {
         return [
             //
-            'nombre' => $this->faker->unique(['Informatica', 'Mecanica', 'Industrial', 'Aeronautica', 'Electronica','Robotica']),
+            'nombre' => $this->faker->randomElement(['Informatica', 'Mecanica', 'Industrial', 'Aeronautica', 'Electronica','Robotica']),
         ];
     }
 }
