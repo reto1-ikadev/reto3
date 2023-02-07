6f4606b10308cc2db3dbce947f765c36de8fb829
celia.garcia1984 <celia.garcia@ikasle.egibide.org>
2023-02-07 13:56:20 +0100

    nombre Año

--- resources/views/historial/show.blade.php
+++ resources/views/historial/show.blade.php
@@ -26,11 +26,11 @@
             @foreach($historial as $fila)
                 <tr class="">
 
-                    <td scope="row">{{$fila->tutores_academicos->persona->nombre}}</td>
-                    <td>R1C2</td>
-                    <td>R1C3</td>
-                    <td>R1C2</td>
-                    <td>R1C3</td>
+                    <td scope="row">{{ $fila->alumnos->persona->nombre }} {{ $fila->alumnos->persona->apellidos }}</td>
+                    <td>{{ $fila->tutores_academicos->persona->nombre }} {{ $fila->tutores_academicos->persona->apellidos }}</td>
+                    <td>{{ $fila->tutores_empresas->persona->nombre }} {{ $fila->tutores_empresas->persona->apellidos }}</td>
+                    <td>{{ $fila->curso->nombre }}</td>
+                    <td>{{ $fila->anos_academicos-> }}</td>
                     <td>R1C2</td>
                     <td>R1C3</td>
                     <td>R1C2</td>
