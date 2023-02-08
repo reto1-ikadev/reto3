bc4b8c51853e7808f8882ccc3cde3eb65eff3413
aritzcas <aritzcas@gmail.com>
2023-02-08 22:56:31 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,21 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/componentes/btnValidar.js',
+                'resources/js/componentes/btnValidarEmpresa.js',
+                'resources/js/formularios/llenarCombos.js',
+                'resources/js/cambiarTutores.js',
+                'resources/js/buscarEstudiantes.js',
+                'resources/js/filtrarDiarios.js',
+                'resources/js/filtrarReuniones.js',
+                'resources/js/calcularMediaNotas.js',
+                'resources/js/detalleEstudiante.js',
+                'resources/js/empresas.js',
+                'resources/js/funciones/validarDatosGrado.js',
+                'resources/js/reuniones_script.js',
+                'resources/js/tutorAcademico.js',
+                'resources/js/buscarEstudiantesTutor.js',
+                'resources/js/tutorEmpresa.js',
 
 
             ],
